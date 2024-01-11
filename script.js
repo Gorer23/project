01ba8e93469fa240af1f19dfd0ed59f4c706561b
Igor <exemple@gmail.com>
2024-01-11 19:50:25 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+"use strict"; //только в начале,помогает ликвидировать ошибки в коде
+
+const categpry = 'toys';
+
+console.log(`https://someurl.com/${categpry}`)
+
+const user = "Ivan";
+alert(`Hello ${user}`); // интерполяция
